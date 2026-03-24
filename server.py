5352ad77ef133e5cac2c753793cc0e9d7aa9b5a3
rafaelreinert83-ai <rafaelreinert83@icloud.com>
2026-03-24 04:51:51 -0300

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,36 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import numpy as np
+import cv2
 
+app = FastAPI()
+
+# LIBERA A COMUNICAÇÃO COM O SITE
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/")
+def home():
+    return {"status": "ok"}
+
+@app.post("/analyze")
+async def analyze(file: UploadFile = File(...)):
+    contents = await file.read()
+    
+    np_arr = np.frombuffer(contents, np.uint8)
+    image = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+    uf = int(200 + np.random.randint(40))
+    hairs = int(uf * 2.2)
+
+    return {
+        "uf": uf,
+        "hairs": hairs,
+        "ratio": round(hairs/uf,2),
+        "score": 88
+    }
